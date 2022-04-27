96642a7e7260ab3d03c8132524260c81aab8cd30
ZhengGongwei <55647822+ZhengGongwei@users.noreply.github.com>
2022-04-27 15:35:33 +0800

    Update plot.R

--- plot.R
+++ plot.R
@@ -24,9 +24,8 @@ FeaturePlot(PBMC, features = ''S100A8'',cols = mycolor, pt.size = 1.5)+theme(pan
 install.packages("remotes")
 remotes::install_github("lyc-1995/MySeuratWrappers")
 library(MySeuratWrappers)
-markers <- c(''CD3D'', ''S100A8'', ''S100A9'', ''CD79A'', ''CCL5'', ''NKG7'', ''GZMA'', ''IL32'', ''CD4'', ''CD8A'', ''LTB'', ''FCN1'', ''MS4A1'', ''SPON2'',''FCER1A'',''SERPINF1'', ''TMEM40'', ''CD3E'')
-my36colors <-c(''#E5D2DD'', ''#53A85F'', ''#F1BB72'', ''#F3B1A0'', ''#D6E7A3'', ''#57C3F3'', ''#476D87'', ''#E95C59'', ''#E59CC4'', ''#AB3282'', ''#23452F'', ''#BD956A'', ''#8C549C'', ''#585658'', ''#9FA3A8'', ''#E0D4CA'', ''#5F3D69'', ''#C5DEBA'', ''#58A4C3'', ''#E4C755'', ''#F7F398'', ''#AA9A59'', ''#E63863'', ''#E39A35'', ''#C1E6F3'', ''#6778AE'', ''#91D0BE'', ''#B53E2B'', ''#712820'', ''#DCC1DD'', ''#CCE0F5'',  ''#CCC9E6'', ''#625D9E'', ''#68A180'', ''#3A6963'', ''#968175'')
-#颜色设置
-VlnPlot(PBMC, features = markers,stacked=T,pt.size=0.9,cols = my36colors,direction = "horizontal",x.lab = '''', y.lab = '''')+theme(axis.text.x = element_blank(),axis.ticks.x = element_blank())#不显示坐标刻度 
+my36colors <-c("#E5D2DD", "#53A85F", "#F1BB72", "#F3B1A0", "#D6E7A3", "#57C3F3", "#476D87", "#E95C59", "#E59CC4", "#AB3282", "#23452F", "#BD956A", "#8C549C", "#585658", "#9FA3A8", "#E0D4CA", "#5F3D69", "#C5DEBA", "#58A4C3")
+markers <- c("CD3D", "S100A8", "S100A9", "CD79A", "CCL5", "NKG7", "GZMA", "IL32", "CD4", "CD8A", "LTB", "FCN1", "MS4A1", "SPON2","FCER1A","SERPINF1", "TMEM40", "CD3E")
+VlnPlot(pbmc3k, features = markers,stack=T,pt.size=0.9,cols = my36colors)+theme(axis.text.x = element_blank(),axis.ticks.x = element_blank())#不显示坐标刻度 
 
 DotPlot(PBMC, features = markers)+coord_flip()+theme_bw()+theme(panel.grid = element_blank(),axis.text.x=element_text(angle=90,hjust = 1,vjust=0.5))+scale_color_gradientn(values = seq(0,1,0.2),ccolours = c(''#330066'',''#336699'',''#66CC66'',''#FFCC33''))+labs(x=NULL,y=NULL)+guides(size=guide_legend(order=3)) 
